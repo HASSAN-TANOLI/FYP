9b12b3f6098a4d8ed7b7390b4c0df99ba7c06785
Shujja <djshujja@gmail.com>
2022-05-18 22:51:27 +0500

    order fix

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,26 +1,36 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 
 const {
-    newOrder,
-    getSingleOrder,
-    myOrders,
-    allOrders,
-    updateOrder,
-    deleteOrder
+  newOrder,
+  getSingleOrder,
+  myOrders,
+  allOrders,
+  updateOrder,
+  deleteOrder,
+  adminOrders,
+} = require("../controllers/orderController");
 
-} = require('../controllers/orderController')
+const {
+  isAuthenticatedUser,
+  isAuthenticatedVendor,
+  authorizeRoles,
+} = require("../middlewares/auth");
 
-const { isAuthenticatedUser, isAuthenticatedVendor, authorizeRoles } = require('../middlewares/auth')
+router.route("/order/new").post(isAuthenticatedUser, newOrder);
 
-router.route('/order/new').post(isAuthenticatedUser, newOrder);
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
+router.route("/orders/user").get(isAuthenticatedUser, myOrders);
 
-router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
-router.route('/orders/user').get(isAuthenticatedUser, myOrders);
-
-router.route('/admin/orders/').get(isAuthenticatedVendor, authorizeRoles('vendor'), allOrders);
-router.route('/admin/order/:id')
-    .put(isAuthenticatedVendor, authorizeRoles('vendor'), updateOrder)
-    .delete(isAuthenticatedVendor, authorizeRoles('vendor'), deleteOrder);
+router
+  .route("/admin/orders/")
+  .get(isAuthenticatedVendor, authorizeRoles("vendor"), allOrders);
+router
+  .route("/admin/admin-orders/")
+  .post(isAuthenticatedVendor, authorizeRoles("vendor"), adminOrders);
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticatedVendor, authorizeRoles("vendor"), updateOrder)
+  .delete(isAuthenticatedVendor, authorizeRoles("vendor"), deleteOrder);
 
 module.exports = router;
